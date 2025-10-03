7c1482e19128b731b5f19869e31829c96d7b5f91
angel <a>
2025-10-02 22:36:25 -0400

    sin  get

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
@@ -26,6 +16,3 @@ Route::get('/controles', [ControleController::class, 'index']);
 Route::get('/delegado/{ci_delegado}', [ControleController::class, 'listaMesasxDelegado']);
 Route::put('/mesa/{id}', [ControleController::class, 'actualizarMesa']);
 Route::post('/adjunto', [AdjuntoController::class, 'store']);
-Route::get('/', function () {
-    return view('welcome'); // O un simple return "¡Backend funcionando!";
-});
